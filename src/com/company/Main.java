f218f84f1960387875b0a872a00c1b648410ab35
greedman231 <93518502+greedman231@users.noreply.github.com>
2021-11-06 12:40:17 +0300

    t-01
    CharArithDemo

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char ch;
+    ch = 'L' ;
+    System.out.println("ch равно" + ch);
+    ch++;
+    System.out.println("значение ch изменилось на " + ch);
+    ch = 'Д';
+    ch--;
+    System.out.println("Значение изменилось и равно " + ch);
+    }
+}
